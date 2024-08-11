726d3f2fe8573b7104dcda12f39cda8750ee207f
NguyennHoangg <nguyenhuyhoang270804@gmail.com>
2024-08-11 21:10:31 +0700

    update

---
+++ BaiTap01/Main.java
@@ -0,0 +1,10 @@
+package BaiTap01;
+
+public class Main {
+    public static void main(String[] args) {
+        ToaDo toaDo = new ToaDo();
+        toaDo.setX(5);
+        toaDo.setY(10);
+        System.out.println(toaDo);
+    }
+}
--- BaiTap01/ToaDo.java
+++ BaiTap01/ToaDo.java
@@ -1,4 +1,32 @@
 package BaiTap01;
 
 public class ToaDo {
+    private int x;
+    private int y;
+
+    public ToaDo() {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "ToaDo{" + "x=" + x + ", y=" + y + '}';
+    }
 }
